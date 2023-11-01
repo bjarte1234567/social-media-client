0fb0d63958c5c0f3ec07c0ef595cad39962259be
bjarte oliversen løken <114422934+bjarte1234567@users.noreply.github.com>
2023-11-01 14:08:59 +0100

    updatetd eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,33 @@ module.exports = {
   extends: ["eslint:recommended"],
   parserOptions: {
     ecmaVersion: 2021,
+{
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
   },
-  
   rules: {
-    // add specific eslint rules here
+    //add any rules for elsint
   },
-  overrides: [
+};
+  "extends": "eslint:recommended",
+  "overrides": [
     {
-      files: ["**/*.js"],
-      rules: {
-        // add rules for test files
+      "files": ["**/*.cy.js"],
+      "env": { "cypress/globals": true },
+      "plugins": ["cypress"],
+      "extends": ["plugin:cypress/recommended"],
+      "rules": {
+        "cypress/no-unnecessary-waiting": "off",
+        "no-unused-vars": "off"
       }
     }
-  ]
-};
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  }
+}
 
   

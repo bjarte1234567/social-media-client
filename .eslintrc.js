e79a26be42570919a2c26851d738a0cc277ffdbd
bjarte oliversen løken <114422934+bjarte1234567@users.noreply.github.com>
2023-11-01 12:40:31 +0100

    added cypress and jest to workflow-jest-cypress branch

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,20 @@
 module.exports = {
-    extends: ["eslint:recommended"],
-    parserOptions: {
-      ecmaVersion: 2021,
-    },
-    rules: {
-      //add any rules for elsint
-    },
-  };
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  
+  rules: {
+    // add specific eslint rules here
+  },
+  overrides: [
+    {
+      files: ["**/*.js"],
+      rules: {
+        // add rules for test files
+      }
+    }
+  ]
+};
+
   

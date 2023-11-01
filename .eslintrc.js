d9214f87cc2ab1779b2132222b7449f949b4ee1b
bjarte oliversen løken <114422934+bjarte1234567@users.noreply.github.com>
2023-11-01 13:58:19 +0100

    updated eslincrc.js with cypress?

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,26 @@
 module.exports = {
-    extends: ["eslint:recommended"],
-    parserOptions: {
-      ecmaVersion: 2021,
+  {
+    "env": {
+      "browser": true,
+      "es2021": true,
+      "node": true
     },
-    rules: {
-      //add any rules for elsint
-    },
-  };
+    "extends": "eslint:recommended",
+    "overrides": [
+      {
+        "files": ["**/*.cy.js"],
+        "env": { "cypress/globals": true },
+        "plugins": ["cypress"],
+        "extends": ["plugin:cypress/recommended"],
+        "rules": {
+          "cypress/no-unnecessary-waiting": "off",
+          "no-unused-vars": "off"
+        }
+      }
+    ],
+    "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
+    }
+  }
   

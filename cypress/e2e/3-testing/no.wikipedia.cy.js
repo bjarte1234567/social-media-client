b435073643558a3db25da4e4915e4d9cd3c17e40
bjarte oliversen løken <114422934+bjarte1234567@users.noreply.github.com>
2023-11-02 11:48:54 +0100

    think ive configured github actions for build if required process task 5

--- cypress/e2e/3-testing/no.wikipedia.cy.js
+++ cypress/e2e/3-testing/no.wikipedia.cy.js
@@ -1,3 +1,4 @@
+//TESTING WIKIPEDIA FOR NOROFF H1
 // describe("no.wikipedia.org", () => {
 //     it("can search for Noroff", () => {
 //       cy.visit("https://no.wikipedia.org");
@@ -5,15 +6,12 @@
 //       cy.get('h1').contains("Noroff")
 //     })
 //   })
-  ////////////////////////////////////
-  
-describe("www.tights.no", () => {
-    it("can search for kreatin", () =>{
-        cy.visit("https://www.tights.no/");
-        cy.get("input#autocomplete-0-input").type("kreatin{enter}", { delay: 500})
-        cy.get(".search-results")
-        .should("contain", "kreatin");
-    });
+////////////////////////////////////
+//TESTING TIGHTS.NO FOR KREATIN SEARCH.
+describe('www.tights.no', () => {
+  it('can search for kreatin', () => {
+    cy.visit('https://www.tights.no/');
+    cy.get('input#autocomplete-0-input').type('kreatin{enter}', { delay: 500 });
+    cy.get('.search-results').should('contain', 'kreatin');
+  });
 });
-  
-  
